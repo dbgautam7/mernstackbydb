191c62bd528f213e30fb7e2aba98e05fe05978bf
D B Gautam <gautamdb28@gmail.com>
2023-01-08 15:23:00 +0545

    password and cionfirm password for register are hashed using bcrypt

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -32,6 +33,16 @@ const userSchema = new mongoose.Schema({
     }
   }
 },{collection:'users'});
+
+userSchema.pre('save', async function(next) { //this is middleware that is executed before save method
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  if (this.isModified('confirmPassword')) {
+    this.confirmPassword = await bcrypt.hash(this.confirmPassword, 10);
+  }
+  next();
+});
 
 const User=mongoose.model('User', userSchema);
 module.exports = User
--- server/router/auth.js
+++ server/router/auth.js
@@ -7,29 +7,84 @@ router.get('/', (req, res) => {
     res.send("Server router is done")
 })
 
-router.post('/register', (req, res) => {
+//using async await promises
+//for register
+router.post('/register', async (req, res) => {
     const { name, email, phone, work, password, confirmPassword } = req.body
 
-    if (!name || !email || !phone || !work || !password || !confirmPassword) {
+    if (!name || !email || !phone || !password || !confirmPassword) {
         return res.status(400).json({ Error: "Please fill the form properly" })
+    }  //work is optional here
+
+    try {
+        
+        const userExists = await User.findOne({ email: email })
+        if (userExists) {
+            return res.status(406).json("Email already existed")
+        }else if(password!==confirmPassword){
+            return res.status(406).json("Password and Confirm Password are not matched")
+        }else{
+            const userDetails = new User({ name, email, phone, work, password, confirmPassword })
+            await userDetails.save()//this code save the userdetails in db
+            res.status(200).json({ message: "User is registered successfully" })
+        }
+        }
+        
+    catch (error) {
+        console.log(error)
     }
-    User.findOne({ email: email })
-        .then((userExists) => {
-            if (userExists) {
-                return res.status(406).json("User already exists")
-            }
-        })
+})
 
-    const user = new User({ name, email, phone, work, password, confirmPassword })
-    user.save().then(() => {
-        res.status(200).json({ message: "User is registered successfully" })
-    }).catch((error) => {
-        res.status(400).json({ error: "Failed to register" })
-    }).catch(error=>console.log(error))
+//using .then promises
+// router.post('/register', (req, res) => {
+//     const { name, email, phone, work, password, confirmPassword } = req.body
 
-    // console.log(password)
-    // res.send("Data sent successfully")
-    // res.json({message:req.body})
+//     if (!name || !email || !phone || !password || !confirmPassword) {
+//         return res.status(400).json({ Error: "Please fill the form properly" })
+//     }  //work is optional here
+//     User.findOne({ email: email })
+//         .then((userExists) => {
+//             if (userExists) {
+//                 return res.status(406).json("User already exists")
+//             }
+//         })
+
+//     const user = new User({ name, email, phone, work, password, confirmPassword })
+//     user.save().then(() => {
+//         res.status(200).json({ message: "User is registered successfully" })
+//     }).catch((error) => {
+//         res.status(400).json({ error: "Failed to register" })
+//     }).catch(error=>console.log(error))
+
+//     // console.log(password)
+//     // res.send("Data sent successfully")
+//     // res.json({message:req.body})
+// })
+
+
+//for signin
+
+router.post('/signin', async (req, res) => {
+
+    const { email: email, password: password } = req.body
+    try {
+        if (!email || !password) {
+            res.status(400).json("Both email and password must be filled")
+        }
+        const userSignin = await User.findOne({ email: email })
+        console.log(userSignin)
+        if (userSignin) {
+            res.status(200).json({ message: "User signin successfully" })
+        } else {
+            res.status(406).json("Unable to signin")
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+    // console.log(req.body)
+    // res.json({message:"signin route created"})
 })
 
 module.exports = router;
